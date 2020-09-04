f82c3a30121820258c05811d18ca516666207cf5
Ramón Reszat <ramon@reszat.de>
2020-09-04 15:32:48 +0200

    Test JUnit setup

---
+++ src/test/java/player/PlayerFactoryTest.java
@@ -0,0 +1,11 @@
+package player;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerFactoryTest {
+    @Test
+    public void testJUnitSetup() {
+        assertTrue(true);
+    }
+}
