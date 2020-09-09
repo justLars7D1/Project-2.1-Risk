e6f0b9554e643ba2d6bb1e922824a487deb6f82d
justLars7D1 <Larsquaedvlieg@outlook.com>
2020-09-09 14:26:55 +0200

    Added edges

--- src/main/java/settings/Settings.java
+++ src/main/java/settings/Settings.java
@@ -9,7 +9,9 @@ public class Settings {
      * The number of countries that exist within the board
      * Here, the index of the country is it's ID (e.g. Alaska -> ID: 0)
      */
-    public static final String[] counties = {"Alaska", // From here North America
+    public static final String[] counties = {
+            // From here North America
+            "Alaska",
             "Alberta (Western Canada)",
             "Central America",
             "Eastern United States",
@@ -18,13 +20,17 @@ public class Settings {
             "Ontario (Central Canada)",
             "Quebec (Eastern Canada)",
             "Western United States",
-            "Great Britain (Great Britain & Ireland)", // From here Europe
+            "Great Britain (Great Britain & Ireland)",
+
+            // From here Europe
             "Iceland",
             "Northern Europe",
             "Scandinavia",
             "Southern Europe",
             "Ukraine (Eastern Europe, Russia)",
-            "Western Europe", // From here Asia
+            "Western Europe",
+
+            // From here Asia
             "Afghanistan",
             "China",
             "India (Hindustan)",
@@ -37,17 +43,23 @@ public class Settings {
             "Siberia",
             "Ural",
             "Yakutsk",
-            "Argentina", // From here South America
+            "Argentina",
+
+            // From here South America
             "Brazil",
             "Peru",
             "Venezuela",
-            "Congo (Central Africa)", // From here Africa
+            "Congo (Central Africa)",
+
+            // From here Africa
             "East Africa",
             "Egypt",
             "Madagascar",
             "North Africa",
             "South Africa",
-            "Eastern Australia", // From here Australia
+            "Eastern Australia", 
+
+            // From here Australia
             "Indonesia",
             "New Guinea",
             "Western Australia"};
@@ -57,6 +69,100 @@ public class Settings {
      * E.g. Alaska <-> Northwest Territory
      * This means that, if an edge exists between them, troops of a country can invade another country
      */
-    public static final String[][] countryEdges = {{"Alaska", "Northwest Territory"} /*etc...*/};
+    public static final String[][] countryEdges = {
+            //Edges within North America
+            {"Alaska", "Northwest Territory"},
+            {"Alaska", "Alberta (Western Canada)"},
+            {"Northwest Territory", "Alberta (Western Canada)"},
+            {"Northwest Territory", "Greenland"},
+            {"Alberta (Western Canada)","Western United States"},
+            {"Alberta (Western Canada)","Ontario (Central Canada)"},
+            {"Ontario (Central Canada)", "Greenland"},
+            {"Ontario (Central Canada)", "Eastern United States" },
+            {"Eastern United States", "Western United States"},
+            {"Eastern United States", "Quebec (Eastern Canada)"},
+            {"Quebec (Eastern Canada)", "Greenland"},
+            {"Western United States", "Central America" },
+
+            //Edges within South America
+            {"Venezuela", "Brazil"},
+            {"Venezuela", "Peru"},
+            {"Peru", "Argentina"},
+            {"Brazil", "Argentina"},
+
+            //Edges within Africa
+            {"North Africa", "Egypt"},
+            {"North Africa", "Congo (Central Africa)"},
+            {"Egypt", "East Africa" },
+            {"Congo (Central Africa)", "East Africa" },
+            {"Congo (Central Africa)", "South Africa"},
+            {"East Africa", "Madagascar"},
+            {"South Africa","Madagascar"},
+
+            //Edges within Europe
+            {"Iceland","Great Britain (Great Britain & Ireland)" },
+            {"Iceland", "Scandinavia"},
+            {"Great Britain (Great Britain & Ireland)","Scandinavia" },
+            {"Scandinavia", "Northern Europe"},
+            {"Scandinavia", "Ukraine (Eastern Europe, Russia)" },
+            {"Great Britain (Great Britain & Ireland)", "Northern Europe" },
+            {"Great Britain (Great Britain & Ireland)", "Western Europe"},
+            {"Western Europe", "Northern Europe"},
+            {"Northern Europe", "Ukraine (Eastern Europe, Russia)"},
+            {"Northern Europe", "Southern Europe"},
+            {"Southern Europe", "Ukraine (Eastern Europe, Russia)"},
+
+            //Edges within Asia
+            {"Middle East","Afghanistan"},
+            {"Middle East", "India (Hindustan)"},
+            {"Afghanistan","Ural"},
+            {"Afghanistan", "India (Hindustan)"},
+            {"Ural","Siberia"},
+            {"India (Hindustan)","China"},
+            {"Ural", "China"},
+            {"Siberia", "China"},
+            {"Siberia", "Mongolia"},
+            {"Siberia", "Irkutsk"},
+            {"Siberia", "Yakutsk"},
+            {"Irkutsk", "Yakutsk"},
+            {"Irkutsk", "Mongolia"},
+            {"Mongolia", "China"},
+            {"China", "Siam (Southeast Asia)"},
+            {"Mongolia", "Japan"},
+            {"Irkutsk", "Kamchatka"},
+            {"Yakutsk", "Kamchatka"},
+            {"Kamchatka", "Japan"},
+
+            //Edges within Australasia
+            {"Indonesia", "New Guinea"},
+            {"Indonesia", "Western Australia"},
+            {"Western Australia", "New Guinea"},
+            {"Western Australia", "Eastern Australia"},
+            {"Eastern Australia", "New Guinea"},
+
+            //Edges between continents are here
+
+            //North America to
+            {"Alaska", "Kamchatka"}, // Asia
+            {"Central America", "Venezuela"},// South America
+            {"Greenland", "Iceland"}, // Europe
+
+            {"Brazil", "North Africa"}, //South America to Africa
+
+            //Africa to
+            {"North Africa", "Western Europe"},//Europe
+            {"North Africa", "Southern Europe"},
+            {"Egypt", "Southern Europe"},
+            {"Egypt", "Middle East"},//Asia
+            {"East Africa", "Middle East"},
+
+            //Europe to
+            {"Ukraine (Eastern Europe, Russia)", "Middle East"},//Asia
+            {"Ukraine (Eastern Europe, Russia)", "Afghanistan"},
+            {"Ukraine (Eastern Europe, Russia)", "Ural"},
+
+            //Asia to
+            {"Siam (Southeast Asia)", "Indonesia"} //Australasia
+    };
 
 }
