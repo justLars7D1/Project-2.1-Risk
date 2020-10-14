9e563fc82b28cd99161d29745b1491976186b029
Joan Botzev <jbotzev@gmail.com>
2020-10-14 11:55:27 +0200

    added rules texts

--- src/main/java/UI/MainMenu/Instructions.java
+++ src/main/java/UI/MainMenu/Instructions.java
@@ -69,44 +69,88 @@ public class Instructions {
     }
 
     private void addRulesPages() {
-         Text page1 = new Text("INTRODUCTION & STRATEGY HINTS\n" +
-                "In the classic World Domination RISK game of military strategy, you are\n" +
-                "battling to conquer the world. To win, you must launch daring attacks,\n" +
-                "defend yourself on all fronts, and sweep across vast continents with\n" +
-                "boldness and cunning. But remember, the dangers, as well as the rewards,\n" +
-                "are high. Just when the world is within your grasp, your opponent might\n" +
-                "strike and take it all away!\n" +
-                "See pages 1l- 16 for gameplay variations and variations for RISK experts.\n" +
-                "Strategy. In all the RISK games, keep these 3 strategy hints in mind as you\n" +
-                "play, add armies, and fortify:\n" +
-                "1.\n" +
-                "2.\n" +
-                "3.\n" +
-                "Conquer whole continents: You will earn more armies that way.\n" +
-                "(This doesnt apply in Secret Mission Risk.)\n" +
-                "Watch your enemies: If they are building up forces on adjacent\n" +
-                "territories or continents, they may be planning an attack. Beware!\n" +
-                "Fortify borders adjacent to enemy territories for better defense if a\n" +
-                "neighbor decides to attack you.");
+        Text page1 = new Text("GOAL OF THE GAME:\n\n" +
+                "Conquer the world by occupying all territories on the board\n" +
+                "  Setup:\n" +
+                " 1. Every player starts with the same amount of infantry units \n" +
+                " a. Canons count as 10 units, cavalry as 5 units, infantry equals 1 unit\n\n" +
 
-        Text page2 = new Text("On your turn, try to capture territories by defeating your opponents’\n" +
-                "armies. But be careful: Winning battles will depend on careful planning,\n" +
-                "quick decisions and bold moves. You’ll have to place your forces wisely,\n" +
-                "attack at just the right time and fortify your defenses against all enemies.\n" +
-                "Note: At any time during the game, you may trade in Infantry pieces for the\n" +
-                "equivalent (see page 4) in Cavalry or Artillery if you need to, or wish to.");
+                " Player	Infantry Units\n" +
+                " 2	40*\n" +
+                " 3	35\n" +
+                " 4	30\n" +
+                " 5	25\n" +
+                " 6	20\n" +
+                "       *special rules for two players\n\n" +
 
-        Text page3 = new Text("On your turn, try to capture territories by defeating your opponents’\n" +
-                "armies. But be careful: Winning battles will depend on careful planning,\n" +
-                "quick decisions and bold moves. You’ll have to place your forces wisely,\n" +
-                "attack at just the right time and fortify your defenses against all enemies.\n" +
-                "Note: At any time during the game, you may trade in Infantry pieces for the\n" +
-                "equivalent (see page 4) in Cavalry or Artillery if you need to, or wish to.\n" +
-                "Note: At any time during the game, you may trade in Infantry pieces for the\n" +
-                "Note: At any time during the game, you may trade in Infantry pieces for the\n" +
-                "Note: At any time during the game, you may trade in Infantry pieces for the\n" +
-                "Note: At any time during the game, you may trade in Infantry pieces for the");
+                "2. Player role a dice to determine who begins. The player who roles the highest number starts by placing one \n" +
+                        "infantry unit onto any territory on the board,\n" +
+                "thus claiming that territory.\n\n" +
 
+                "3. Starting to the left of the first player, in turn, everyone places one army onto any\n" +
+                        "unoccupied territory. Continue until all 42 territories have been claimed.\n\n" +
+
+                "4. After all the 42 territories are claimed, each player in turn places one additional army onto\n" +
+                        "any territory he or she already occupies.\n" +
+                "Continue in this way until everyone has run out of armies. There is no limit to the number of\n" +
+                        "armies you may place onto a single territory.\n\n" +
+
+                "5. After all armies are set the player who placed the first army opens the game");
+
+        Text page2 = new Text(
+                "GAMEPLAY\n\n" +
+
+                "Every player takes turns. A turn consists of the following actions:\n\n" +
+
+                "1.Each player receives new infantry units which he can place on territories he occupies.The number of armies \n" +
+                " received depends on the following conditions\n\n" +
+
+                "1. TERRITORIES\n\n" +
+
+                "Divide the number of currently occupied territories by three ignore the fraction. (The player\n" +
+                "receives at least 3 infantry units)\n\n" +
+                "2. CONTINENTS\n\n" +
+
+                "A player receives bonus units if he controls every territory of a continent. \n" +
+                "The number of additional units depends on the occupied continent\n" +
+                "Continent	Infantry Units\n" +
+                "Asia................7\n" +
+                "North America.......5\n" +
+                "Europe..............5\n" +
+                "Africa..............3\n" +
+                "South America.......2\n" +
+                "Australia...........2\n\n"
+
+        );
+
+        Text page3 = new Text(
+                "After placing infantry units, the player may attack.\n\n" +
+                "-You may only attack a territory that is adjacent (touching) to one of your own, or \n" +
+                "connected to it by a dashed line.\n" +
+
+                "-You must always have at least two armies in the territory you’re attacking from.\n" +
+
+                "-You may continue attacking one territory until you have eliminated all armies on it,\n" +
+                "or you may shift your attack from on territory or another,\n" +
+                "attacking each as often as you like and attacking as many territories as you like during one turn.\n\n" +
+
+                "HOW TO ATTACK\n\n" +
+                "-Before rolling, both you and your opponent must announce the number of dice you \n" +
+                "intend to roll, and you both must roll at the same time.\n" +
+                "-For simplicity, each player will always attack and defend with the maximum number of armies available.\n" +
+
+                "-You, the attacker, will roll 1,2 or 3 red dice: You must have at least one more army in your\n" +
+                " territory than the number of dice you roll. \n" +
+
+                "-The defender will roll either 1 or 2 white dice: To roll 2 dice, he or she must have at least\n" +
+                " 2 armies on the territory under attack. \n" +
+
+                "-Compare the highest die each of you rolled. If yours (the attacker's) is higher, the defender\n" +
+                "loses one army from the territory under attack.\n" +
+                "But if the defender's die is higher than yours, you lose one army from the territory\n" +
+                "you attacked from.\n" +
+                "If each of you rolled more than one die, now compare the two next-highest dice and repeat the process.\n" +
+                " (In case if a tie, the defender wins)");
         rules = new ArrayList<>();
         rules.add(page1);
         rules.add(page2);
