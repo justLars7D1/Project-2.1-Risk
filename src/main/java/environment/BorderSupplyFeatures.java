f75d86744cd625841c1db5bac7ee00893e2f2539
Ramón Reszat <ramon@reszat.de>
2020-11-06 15:18:50 +0100

    Add supply feature class

---
+++ src/main/java/environment/BorderSupplyFeatures.java
@@ -0,0 +1,10 @@
+import gameelements.game.Game;
+import gameelements.board.Country;
+import gameelements.player.Player;
+import gameelements.player.PlayerType;
+
+import java.util.*;
+
+public class BorderSupplyFeatures {
+    
+}
