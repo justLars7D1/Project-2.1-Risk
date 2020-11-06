ddb9e5d2fb191aab322b620dc6dfaf4d186954da
julio-py <roeder_daniel_97@web.de>
2020-11-06 15:11:36 +0100

    feature method inside test class

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,11 @@
+import gameelements.board.Country;
 import gameelements.game.Game;
+import gameelements.player.Player;
 import gameelements.player.PlayerType;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
@@ -13,5 +17,31 @@ public class test {
         System.out.println(game.getGamePhase());
         System.out.println(game.getBattlePhase());
 
+        System.out.println(game.getCurrentPlayer());
+
+
     }
+    public static void get_state_features(Game game){
+        Player current_player = game.getCurrentPlayer();
+        HashSet<Country> owned_countries = current_player.getCountriesOwned();
+        int bst;
+        double bsr;
+
+        for (Country country : owned_countries) {
+            bst = 0;
+            bsr = 0;
+            int player_troops = country.getNumSoldiers();
+
+            List<Country> neighbor_countries = country.getNeighboringCountries();
+            for (Country n_country : neighbor_countries) {
+                if (n_country.getOwner() != null && n_country.getOwner() != current_player) {
+                    bst += n_country.getNumSoldiers();
+                }
+            }
+            bsr = bst / player_troops;
+            System.out.println("BST for country" + country + " equals " +bst);
+            System.out.println("BSR for country" + country + " equals " +bsr);
+        }
+    }
+
 }
