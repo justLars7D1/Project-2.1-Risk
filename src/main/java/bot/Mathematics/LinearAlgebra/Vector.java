d3bb00158720037116a049eef445e0f63f2fe8f5
Ramón Reszat <ramon@reszat.de>
2020-11-20 18:04:06 +0100

    Use sigmoid output units

--- src/main/java/bot/Mathematics/LinearAlgebra/Vector.java
+++ src/main/java/bot/Mathematics/LinearAlgebra/Vector.java
@@ -226,13 +226,6 @@ public class Vector implements Cloneable, Serializable {
         return copyOfCurrentVector;
     }
 
-    public void sigmoid(){
-        int dimensions = getDimensions();
-        for (int i = 0; i < dimensions; i++) {
-            coordinates[i] = 1 / (1 + Math.exp(coordinates[i]));
-        }
-    }
-
     @Override
     public Object clone() {
         return new Vector(coordinates.clone());
